7db695fe84c7a76d46eac0220e505ed6ba225372
Kevin Zakka <kevinarmandzakka@gmail.com>
2021-07-31 14:30:00 -0700

    Add dataclasses.

--- fusion.py
+++ fusion.py
@@ -1,10 +1,40 @@
-import dataclasses
+import math
+import time
+from dataclasses import dataclass
+from typing import Tuple
 
-import jax.numpy as jnp
 import numpy as np
 
+# Global constants.
+BYTES_TO_MEGABYTES = 1.0 / 1024.0
+BYTES_TO_GIGABYTES = 1.0 / 1024.0 / 1024.0
 
-@dataclasses.dataclass(frozen=True)
+# Global types.
+Int3 = Tuple[int, int, int]
+
+
+@dataclass
+class GlobalConfig:
+    """The global configuration."""
+
+    volume_size: Int3 = (512, 512, 512)
+    """The overall size of the volume in mm."""
+
+    voxel_scale: float = 2.0
+    """The volume discretization in mm."""
+
+    truncation_distance: float = 25.0
+    """The truncation distance for updating the TSDF volume."""
+
+    depth_cutoff_distance: float = 3000.0
+    """The distance in mm after which the depth is set to 0 in the depth frames."""
+
+    triangles_buffer_size: int = 3 * 2000000
+    pointcloud_buffer_size: int = 3 * 2000000
+    """The maximum buffer size for exporting triangles and pointclouds."""
+
+
+@dataclass(frozen=True)
 class Intrinsic:
     """Pinhole camera parameters."""
 
@@ -16,77 +46,149 @@ class Intrinsic:
     cy: float
 
     @property
-    def matrix(self) -> jnp.ndarray:
-        return jnp.array(
+    def matrix(self) -> np.ndarray:
+        return np.array(
             [
-                [self.fx, 0, self.cx],
-                [0, self.fy, self.cy],
+                [self.fx, 0.0, self.cx],
+                [0.0, self.fy, self.cy],
                 [0.0, 0.0, 1.0],
             ],
-            dtype=jnp.float32,
+            dtype=np.float32,
         )
 
 
-@dataclasses.dataclass(frozen=True)
-class Frame:
-    color: jnp.ndarray
-    """An RGB image, of shape (H, W, 3)."""
+class UniformTSDFVolume:
+    """A TSDF volume with a uniform voxel grid [1].
 
-    depth: jnp.ndarray
-    """A depth image, of shape (H, W)."""
+    References:
+        [1]: Curless and Levoy, 1996.
+    """
 
-    intrinsic: Intrinsic
-    """Pinhole camera intrinsic parameters, of shape (3, 3)."""
-
-    extrinsic: jnp.ndarray
-    """Camera pose, of shape (4, 4)."""
-
-
-def get_view_frustum(
-    depth_im: np.ndarray,
-    intr: Intrinsic,
-    extr: np.ndarray,
-) -> np.ndarray:
-    """Get the corners of the camera frustum from the depth image."""
-    pass
-
-
-def rigid_transform(xyz, transform):
-    """Applies an SE(3) transform on a pointcloud."""
-    # Homogenize coordinates.
-    # Matrix multiplication.
-    pass
-
-
-
-class TSDFVolume:
-    """Volumetric TSDF Fusion of RGB-D Images."""
-
-    def __init__(
-        self,
-        volume_bounds,
-        voxel_size: float,
-        sdf_truncation: float,
-    ) -> None:
+    def __init__(self, volume_size: Int3, voxel_scale: float):
         """Constructor.
 
         Args:
-            volume_bounds:
-            voxel_size:
-            sdf_truncation:
+            volume_size: The dimensions of the 3D volume in mm. Will be allocated on
+                the compute device so should be decreased for low RAM.
+            voxel_scale: Controls the resolution of the volume. Lowering this value
+                makes a high-resolution TSDF volume, but will make the integration
+                susceptible to depth noise.
         """
+        self.volume_size = volume_size
+        self.voxel_scale = voxel_scale
+        self._tsdf_volume = None
+        self._color_volume = None
+
+    def reset(self):
+        self._tsdf_volume = np.zeros(self.volume_size + (2,), dtype=np.float32)
+        self._color_volume = np.zeros(self.volume_size, dtype=np.uint8)
+
+        print(f"TSDF volume: {self._tsdf_volume.nbytes * BYTES_TO_GIGABYTES} GB")
+
+    @property
+    def tsdf_volume(self) -> np.ndarray:
+        return self._tsdf_volume
+
+    @property
+    def color_volume(self) -> np.ndarray:
+        return self._color_volume
+
+
+class TSDFFusion:
+    """Volumetric TSDF Fusion of RGB-D Images."""
+
+    def __init__(self, camera_params: Intrinsic, config: GlobalConfig):
+        """Set up the internal volume and camera parameters."""
+
+        self._camera_params = camera_params
+        self._config = config
+        self._volume = UniformTSDFVolume(config.volume_size, config.voxel_scale)
+
         self.reset()
 
-    def reset(self) -> None:
-        """Reset the volume."""
+    def reset(self):
+        self._volume.reset()
 
-    def integrate(self, frame: Frame, weight: float) -> None:
-        """Integrate an RGB-D frame into the TSDF volume.
+    def integrate(
+        self,
+        color_im,
+        depth_im,
+        pose,
+    ):
+        """Integrate an RGB-D frame into the TSDF volume."""
 
-        Args:
-            color_im:
-            depth_im:
-            cam_intr:
-            cam_pose:
-            weight:
-        """
+        # Sanity check shapes.
+        assert pose.shape == (4, 4)
+        assert color_im.shape[:2] == depth_im.shape[:2]
+        if (
+            depth_im.shape[0] != self._camera_params.height
+            or depth_im.shape[1] != self._camera_params.width
+        ):
+            raise ValueError(f"Depth image size does not match camera parameters.")
+
+        # Fuse the RGB-D frame into the volume.
+        surface_reconstruction(
+            depth_im,
+            color_im,
+            self._volume,
+            self._camera_params,
+            self._config.truncation_distance,
+            se3_inverse(pose),
+        )
+
+    def extract_pointcloud(self):
+        return extract_points(
+            self._volume,
+            self._config.pointcloud_buffer_size,
+        )
+
+    def extract_mesh(self):
+        return marching_cubes(
+            self._volume,
+            self._config.triangles_buffer_size,
+        )
+
+
+def se3_inverse(pose: np.ndarray) -> np.ndarray:
+    """A faster SE3 inverse."""
+    inv_pose = np.empty_like(pose)
+    tr = pose[:3, :3].T
+    inv_pose[:3, :3] = tr
+    inv_pose[:3, 3] = -tr @ pose[:3, 3]
+    inv_pose[3, :] = [0, 0, 0, 1.0]
+    return inv_pose
+
+
+def surface_reconstruction(
+    depth_image: np.ndarray,
+    color_image: np.ndarray,
+    volume: UniformTSDFVolume,
+    cam_params: Intrinsic,
+    truncation_distance: float,
+    pose: np.ndarray,
+):
+    """Integration of surface measurements into a global volume."""
+
+    # Compute xyz coordinates using voxel_scale.
+
+    # Convert to camera coordinates.
+
+    # If z is <= 0 remove.
+
+    # Convert to pixel coordinates using intrinsic.
+
+    # Eliminate pixels outside view frustum.
+
+    # Get depth[u, v]. If <=0, remove.
+
+    # Reproject and unit normalize.
+
+    # Compute sdf.
+
+
+def extract_points(volume: UniformTSDFVolume, buffer_size: int):
+    """Extract a pointcloud from a TSDF volume."""
+
+
+def marching_cubes(volume: UniformTSDFVolume, buffer_size: int):
+    """Extract a surface mesh from a TSDF volume using the Marching Cubes algorithm."""
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import dataclasses
-import time
 from pathlib import Path
 from typing import Union
 
@@ -13,25 +11,12 @@ import fusion
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string("path", "data/", "Path to dataset.")
-flags.DEFINE_float(
-    "volume_bounds_frac", 0.05, "Fraction of the data to estimate volume bounds on."
-)
-
-# flags.mark_flag_as_required("experiment_name")
+flags.DEFINE_string("path", "data", "Path to dataset.")
 
 # Path templates.
 DEPTH_PATH = "frame-{:06}.depth.png"
 COLOR_PATH = "frame-{:06}.color.jpg"
 POSE_PATH = "frame-{:06}.pose.txt"
-
-# @dataclasses.dataclass(frozen=True)
-# class Voxel:
-#     voxel_size: float
-#     """The volume discretization in meters."""
-
-#     volume_bounds: jnp.ndarray
-#     """The Cartesian bounds (min/max) in meters."""
 
 
 def load_intrinsics(filename: Union[str, Path]) -> fusion.Intrinsic:
@@ -48,9 +33,8 @@ def load_intrinsics(filename: Union[str, Path]) -> fusion.Intrinsic:
 
 def load_depth(filename: Union[str, Path]) -> np.ndarray:
     img = np.array(Image.open(filename), dtype=np.float32)
-    img /= 1000.0
-    img[img == 65.535] = 0
-    return img
+    img[img == 65535] = 0.0
+    return np.array(img)  # Values are in millimeters.
 
 
 def load_color(filename: Union[str, Path]) -> np.ndarray:
@@ -58,28 +42,21 @@ def load_color(filename: Union[str, Path]) -> np.ndarray:
 
 
 def load_pose(filename: Union[str, Path]) -> np.ndarray:
-    return np.loadtxt(filename).astype(np.float32)
+    return np.array(np.loadtxt(filename), dtype=np.float64)
 
 
-def get_convex_hull(
-    path: Path, intr: fusion.Intrinsic, max_frames: int = -1
-) -> np.ndarray:
+def get_convex_hull(path: Path, intr: fusion.Intrinsic) -> np.ndarray:
     """Compute extrema of convex hull of camera view frustums."""
     depth_filenames = list(path.glob("*depth.png"))
     pose_filenames = list(path.glob("*pose.txt"))
-    if max_frames != -1:
-        np.random.shuffle(depth_filenames)
-        np.random.shuffle(pose_filenames)
-        depth_filenames = depth_filenames[:max_frames]
-        pose_filenames = pose_filenames[:max_frames]
     n_files = len(pose_filenames)
     vol_bounds = np.zeros((3, 2))
-    for depth_f, pose_f in tqdm(zip(depth_filenames, pose_filenames), total=n_files):
-        depth_im = load_depth(depth_f)
-        cam_pose = load_pose(pose_f)
-        view_frustum = fusion.get_view_frustum(depth_im, intr, cam_pose)
-        vol_bounds[:, 0] = np.minimum(vol_bounds[:, 0], np.amin(view_frustum, axis=1))
-        vol_bounds[:, 1] = np.maximum(vol_bounds[:, 1], np.amax(view_frustum, axis=1))
+    # for depth_f, pose_f in tqdm(zip(depth_filenames, pose_filenames), total=n_files):
+    # depth_im = load_depth(depth_f)
+    # cam_pose = load_pose(pose_f)
+    # view_frustum = fusion.get_view_frustum(depth_im, intr, cam_pose)
+    # vol_bounds[:, 0] = np.minimum(vol_bounds[:, 0], np.amin(view_frustum, axis=1))
+    # vol_bounds[:, 1] = np.maximum(vol_bounds[:, 1], np.amax(view_frustum, axis=1))
     return vol_bounds
 
 
@@ -87,20 +64,24 @@ def main(_):
     path = Path(FLAGS.path)
     assert path.exists(), "f{path} does not exist."
 
-    # Figure out the total number of frames.
-    n_total = len(list(path.glob("*jpg")))
-
     # Load camera intrinsic parameters.
     intr = load_intrinsics(path / "camera-intrinsics.txt")
 
-    # Compute volume bounds in world coordindates of the convex hull of camera view
-    # frustums in the dataset.
-    volume_bounds = get_convex_hull(
-        path,
-        intr,
-        int(FLAGS.volume_bounds_frac * n_total),
-    )
+    # Instantiate global config.
+    config = fusion.GlobalConfig()
+    config.volume_scale = 0.04
 
+    # Instantiate fusion pipeline with camera parameters and global config.
+    pipeline = fusion.TSDFFusion(intr, config)
+
+    # Loop through the RGB-D frames and fuse.
+    n_frames = 3
+    for i in tqdm(range(n_frames)):
+        pipeline.integrate(
+            load_color(path / COLOR_PATH.format(i)),
+            load_depth(path / DEPTH_PATH.format(i)),
+            load_pose(path / POSE_PATH.format(i)),
+        )
 
 
 if __name__ == "__main__":
