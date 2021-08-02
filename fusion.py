e8e87993d29d48e3890a5a6579428a96885cf664
Kevin Zakka <kevinarmandzakka@gmail.com>
2021-08-01 19:50:25 -0700

    TSDF and color integration semi working.

--- fusion.py
+++ fusion.py
@@ -1,12 +1,19 @@
+from __future__ import annotations
+
+import time
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Tuple, Union
 
 import numpy as np
+from ipdb import set_trace
+from skimage.measure import marching_cubes
 
 # Global constants.
-BYTES_TO_MEGABYTES = 1.0 / 1024.0
-BYTES_TO_GIGABYTES = 1.0 / 1024.0 / 1024.0
+BYTES_TO_GIGABYTES = 1.0 / (1 << 30)
+SHORTMAX = np.iinfo(np.int16).max
+DIVSHORTMAX = 0.0000305185
+MAX_WEIGHT = 128
 
 # Global types.
 Int3 = Tuple[int, int, int]
@@ -19,18 +26,14 @@ class GlobalConfig:
     volume_size: Int3 = (512, 512, 512)
     """The overall size of the volume in mm."""
 
-    voxel_scale: float = 2.0
+    voxel_scale: float = 20.0
     """The volume discretization in mm."""
 
-    truncation_distance: float = 25.0
-    """The truncation distance for updating the TSDF volume."""
+    truncation_distance: float = 100.0
+    """The truncation distance (in mm) for updating the TSDF volume."""
 
-    depth_cutoff_distance: float = 3000.0
-    """The distance in mm after which the depth is set to 0 in the depth frames."""
-
-    triangles_buffer_size: int = 3 * 2000000
-    pointcloud_buffer_size: int = 3 * 2000000
-    """The maximum buffer size for exporting triangles and pointclouds."""
+    depth_cutoff_distance: float = 4000.0
+    """The distance (in mm) after which the depth is set to 0 in the depth frames."""
 
 
 @dataclass(frozen=True)
@@ -57,11 +60,8 @@ class Intrinsic:
 
     @classmethod
     def from_file(
-        cls,
-        filename: Union[str, Path],
-        width: int,
-        height: int,
-    ) -> "Intrinsic":
+        cls, filename: Union[str, Path], width: int, height: int
+    ) -> Intrinsic:
         mat = np.loadtxt(filename, delimiter=" ", dtype=np.float32)
         return cls(width, height, mat[0, 0], mat[1, 1], mat[0, 2], mat[1, 2])
 
@@ -85,22 +85,37 @@ class UniformTSDFVolume:
         """
         self.volume_size = volume_size
         self.voxel_scale = voxel_scale
+
         self._tsdf_volume = None
+        self._weight_volume = None
         self._color_volume = None
 
     def reset(self):
-        self._tsdf_volume = np.zeros(self.volume_size + (2,), dtype=np.float32)
-        self._color_volume = np.zeros(self.volume_size, dtype=np.uint8)
+        self._tsdf_volume = np.zeros(self.volume_size, dtype=np.int16)
+        self._weight_volume = np.zeros(self.volume_size, dtype=np.int16)
+        self._color_volume = np.zeros(self.volume_size + (3,), dtype=np.uint8)
 
-        print(f"TSDF volume: {self._tsdf_volume.nbytes * BYTES_TO_GIGABYTES} GB")
+        size = 0
+        for volume in [self._tsdf_volume, self._weight_volume, self._color_volume]:
+            size += volume.nbytes
+        size *= BYTES_TO_GIGABYTES
+        print(f"TSDF volume: {size:.4} GB")
 
     @property
     def tsdf_volume(self) -> np.ndarray:
         return self._tsdf_volume
 
     @property
+    def weight_volume(self) -> np.ndarray:
+        return self._weight_volume
+
+    @property
     def color_volume(self) -> np.ndarray:
         return self._color_volume
+
+    @property
+    def center_point(self) -> np.ndarray:
+        return np.asarray(self.volume_size, dtype=np.float32) * 0.5 * self.voxel_scale
 
 
 class TSDFFusion:
@@ -116,6 +131,7 @@ class TSDFFusion:
         self.reset()
 
     def reset(self):
+        self._frame_id = 0
         self._volume.reset()
 
     def integrate(
@@ -135,6 +151,9 @@ class TSDFFusion:
         ):
             raise ValueError(f"Depth image size does not match camera parameters.")
 
+        # Truncate depth values >= than the cutoff.
+        depth_im[depth_im >= self._config.depth_cutoff_distance] = 0.0
+
         # Fuse the RGB-D frame into the volume.
         surface_reconstruction(
             depth_im,
@@ -145,17 +164,17 @@ class TSDFFusion:
             se3_inverse(pose),
         )
 
+        self._frame_id += 1
+
     def extract_pointcloud(self):
-        return extract_points(
-            self._volume,
-            self._config.pointcloud_buffer_size,
-        )
+        return extract_points(self._volume)
 
     def extract_mesh(self):
-        return marching_cubes(
-            self._volume,
-            self._config.triangles_buffer_size,
-        )
+        return extract_mesh(self._volume)
+
+    @property
+    def frame_id(self) -> int:
+        return self._frame_id
 
 
 def se3_inverse(pose: np.ndarray) -> np.ndarray:
@@ -169,28 +188,137 @@ def se3_inverse(pose: np.ndarray) -> np.ndarray:
 
 
 def surface_reconstruction(
-    depth_image: np.ndarray,
-    color_image: np.ndarray,
+    depth_im: np.ndarray,
+    color_im: np.ndarray,
     volume: UniformTSDFVolume,
-    cam_params: Intrinsic,
+    intr: Intrinsic,
     truncation_distance: float,
     pose: np.ndarray,
 ):
     """Integration of surface measurements into a global volume."""
 
-    # Compute xyz coordinates using voxel_scale.
-    # Convert to camera coordinates.
-    # If z is <= 0 remove.
-    # Convert to pixel coordinates using intrinsic.
+    # Create voxel grid coordinates.
+    # tic = time.time()
+    vox_coords = np.indices(volume.volume_size).reshape(3, -1).T
+    # print(f"voxel creation: {time.time() - tic}s")
+
+    # Convert voxel center from grid coordinates to base frame camera coordinates.
+    # tic = time.time()
+    world_pts = ((vox_coords.astype(np.float32) + 0.5) * volume.voxel_scale) - volume.center_point
+    # print(f"world coords: {time.time() - tic}s")
+
+    # Convert from base frame camera coordinates to current frame camera coordinates
+    # tic = time.time()
+    cam_pts = rigid_transform(world_pts, pose)
+    # print(f"rigid transform: {time.time() - tic}s")
+
+    # Remove points with negative z.
+    # tic = time.time()
+    mask = cam_pts[:, 2] > 0
+    # print(f"invalid z mask: {time.time() - tic}s")
+
+    # Convert to camera pixels.
+    # tic = time.time()
+    pix_x = np.full_like(cam_pts[:, 0], -1, dtype=np.int32)
+    pix_y = np.full_like(cam_pts[:, 0], -1, dtype=np.int32)
+    pix_x[mask] = np.around(
+        cam_pts[mask, 0] / cam_pts[mask, 2] * intr.fx + intr.cx
+    ).astype(np.int32)
+    pix_y[mask] = np.around(
+        cam_pts[mask, 1] / cam_pts[mask, 2] * intr.fy + intr.cy
+    ).astype(np.int32)
+    # print(f"projection: {time.time() - tic}s")
+
     # Eliminate pixels outside view frustum.
-    # Get depth[u, v]. If <=0, remove.
-    # Reproject and unit normalize.
-    # Compute sdf.
+    # tic = time.time()
+    mask &= (pix_x >= 0) & (pix_x < intr.width) & (pix_y >= 0) & (pix_y < intr.height)
+    # print(f"invalid pix mask: {time.time() - tic}s")
+
+    # tic = time.time()
+    depth_val = np.zeros_like(pix_x, dtype=np.float32)
+    depth_val[mask] = depth_im[pix_y[mask], pix_x[mask]]
+    # print(f"index depth values: {time.time() - tic}s")
+
+    # tic = time.time()
+    mask &= depth_val > 0
+    # print(f"positive only depth mask: {time.time() - tic}s")
+
+    # tic = time.time()
+    depth_diff = depth_val - cam_pts[:, 2]
+    valid_pts = (depth_val > 0) & (depth_diff >= -truncation_distance)
+    dist = np.minimum(1.0, depth_diff / truncation_distance)
+    valid_vox_x = vox_coords[valid_pts, 0]
+    valid_vox_y = vox_coords[valid_pts, 1]
+    valid_vox_z = vox_coords[valid_pts, 2]
+    valid_dist = dist[valid_pts]
+
+    tsdf_vals = volume.tsdf_volume[valid_vox_x, valid_vox_y, valid_vox_z]
+    tsdf_vals = tsdf_vals.astype(np.float32) * DIVSHORTMAX
+
+    w_old = volume.weight_volume[valid_vox_x, valid_vox_y, valid_vox_z]
+    obs_weight = 1
+
+    tsdf_vol_new = (w_old * tsdf_vals + obs_weight * valid_dist) / (w_old + obs_weight)
+    tsdf_vol_new = np.clip(
+        (tsdf_vol_new * SHORTMAX).astype(np.int16),
+        a_min=-SHORTMAX,
+        a_max=SHORTMAX,
+    )
+    volume.tsdf_volume[valid_vox_x, valid_vox_y, valid_vox_z] = tsdf_vol_new
+
+    # From paper: Moreover, by truncating the updated weight over some value MAX_WEIGHT,
+    # a moving average surface reconstruction can be obtained enabling reconstruction in
+    # scenes with dynamic object motion.
+    w_new = np.minimum(w_old + obs_weight, MAX_WEIGHT)
+    volume.weight_volume[valid_vox_x, valid_vox_y, valid_vox_z] = w_new
+    # print(f"tsdf integration: {time.time() - tic}s")
+
+    # tic = time.time()
+    for i in range(3):
+        volume.color_volume[valid_vox_x, valid_vox_y, valid_vox_z, i] = (
+            (w_old * volume.color_volume[valid_vox_x, valid_vox_y, valid_vox_z, i] \
+                + obs_weight * color_im[pix_y[valid_pts], pix_x[valid_pts], i]) \
+                    / (w_old + obs_weight)).astype(np.uint8)
+    # print(f"color integration: {time.time() - tic}s")
 
 
-def extract_points(volume: UniformTSDFVolume, buffer_size: int):
+    # color_const = 256 * 256
+    # color_im = color_im.astype(np.float32)
+    # color_im = np.floor(color_im[..., 2] * color_const + color_im[..., 1] * 256 + color_im[..., 0])
+    # old_color = volume.color_volume[valid_vox_x, valid_vox_y, valid_vox_z]
+    # old_b = np.floor(old_color / color_const)
+    # old_g = np.floor((old_color - old_b * color_const) / 256)
+    # old_r = old_color - old_b * color_const - old_g * 256
+    # new_color = color_im[pix_y[valid_pts], pix_x[valid_pts]]
+    # new_b = np.floor(new_color / color_const)
+    # new_g = np.floor((new_color - new_b * color_const) / 256)
+    # new_r = new_color - new_b * color_const - new_g * 256
+    # new_b = np.minimum(255.0, np.round((w_old * old_b + obs_weight * new_b) / w_new))
+    # new_g = np.minimum(255.0, np.round((w_old * old_g + obs_weight * new_g) / w_new))
+    # new_r = np.minimum(255.0, np.round((w_old * old_r + obs_weight * new_r) / w_new))
+    # volume.color_volume[valid_vox_x, valid_vox_y, valid_vox_z] = (
+    #     new_b * color_const + new_g * 256 + new_r
+    # )
+    # print(f"color integration: {time.time() - tic}s")
+
+    # print(f"total: {time.time() - start}s")
+
+
+def extract_points(volume: UniformTSDFVolume):
     """Extract a pointcloud from a TSDF volume."""
+    raise NotImplementedError
 
 
-def marching_cubes(volume: UniformTSDFVolume, buffer_size: int):
+def extract_mesh(volume: UniformTSDFVolume):
     """Extract a surface mesh from a TSDF volume using the Marching Cubes algorithm."""
+    tsdf_volume = volume.tsdf_volume.astype(np.float32) * DIVSHORTMAX
+    mask = (tsdf_volume > -0.5) & (tsdf_volume < 0.5)
+    verts, faces, norms, _ = marching_cubes(tsdf_volume, mask=mask, level=0)
+    verts_ind = np.round(verts).astype(int)
+    verts = verts * volume.voxel_scale - volume.center_point
+    colors = volume.color_volume[verts_ind[:, 0], verts_ind[:, 1], verts_ind[:, 2]]
+    return verts, faces, norms, colors
+
+
+def rigid_transform(xyz, transform):
+    return (transform[:3, :3] @ xyz.T).T + transform[:3, 3]
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from os import pipe
 from pathlib import Path
 
+import numpy as np
 from absl import app, flags
 from tqdm import tqdm
 
@@ -26,19 +28,26 @@ def main(_):
 
     # Instantiate global config.
     config = fusion.GlobalConfig()
-    config.volume_scale = 0.04
+    config.volume_size = (471, 289, 292)  # (512, 512, 512)
+    config.voxel_scale = 0.02 * 1000.0
+    config.truncation_distance = 5 * config.voxel_scale
+    config.depth_cutoff_distance = 4.0 * 1000.0
+    print(config)
 
     # Instantiate fusion pipeline with camera parameters and global config.
     pipeline = fusion.TSDFFusion(intr, config)
 
     # Loop through the RGB-D frames and fuse.
-    n_frames = 3
+    n_frames = 1
     for i in tqdm(range(n_frames)):
         pipeline.integrate(
             utils.load_color(path / COLOR_PATH.format(i)),
             utils.load_depth(path / DEPTH_PATH.format(i)),
             utils.load_pose(path / POSE_PATH.format(i)),
         )
+
+    mesh_args = pipeline.extract_mesh()
+    utils.meshwrite("./mesh.ply", *mesh_args)
 
 
 if __name__ == "__main__":
